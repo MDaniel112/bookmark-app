f1728b95d4b81ea91585f3781862b94b621b4b84
MDaniel112 <dtmocanu112@gmail.com>
2025-06-25 17:18:56 +0300

    first commit

---
+++ backend/src/test/java/com/bookmarkapp/bookmark/BookmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmarkapp.bookmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
